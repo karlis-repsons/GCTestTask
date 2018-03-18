f3bc964eae462cd57e2b804eb1d4dae02b7d9e01
Kārlis Repsons <karlis.repsons@gmail.com>
2018-03-18 23:50:53 +0200

    Small improvements in WindowsOS.Lib.

--- WindowsOS.Lib/Drivers/Installed/_share_/dataTypes/DriverPropName.cs
+++ WindowsOS.Lib/Drivers/Installed/_share_/dataTypes/DriverPropName.cs
@@ -1,11 +1,20 @@
-﻿namespace WindowsOS.Lib.Drivers.Installed
+﻿using System.Diagnostics.Contracts;
+
+namespace WindowsOS.Lib.Drivers.Installed
 {
     /// <summary>
     /// Must stay immutable.
     /// </summary>
     public class DriverPropName
     {
-        public DriverPropName(string str) => this.name = str;
+        public DriverPropName(string name) {
+            Contract.Requires<InvalidDriverPropName>(
+                        string.IsNullOrEmpty(name) == false,
+                        "Null or empty not ok."
+            );
+
+            this.name = name;
+        }
 
         public static implicit operator string(DriverPropName dpn) => dpn.name;
 
--- WindowsOS.Lib/Drivers/_share_/dataTypes/DriverModuleName.cs
+++ WindowsOS.Lib/Drivers/_share_/dataTypes/DriverModuleName.cs
@@ -1,11 +1,20 @@
-﻿namespace WindowsOS.Lib.Drivers
+﻿using System.Diagnostics.Contracts;
+
+namespace WindowsOS.Lib.Drivers
 {
     /// <summary>
     /// Must stay immutable.
     /// </summary>
     public class DriverModuleName
     {
-        public DriverModuleName(string str) => this.name = str;
+        public DriverModuleName(string name) {
+            Contract.Requires<InvalidDriverModuleName>(
+                        string.IsNullOrEmpty(name) == false,
+                        "Null or empty not ok."
+            );
+
+            this.name = name;
+        }
 
         public static implicit operator string(DriverModuleName dmn)
                                                     => dmn.name;
